ae78316475c91fc7bcd4462d4699e5909274b8c6
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-02-12 18:49:30 -0500

    Add nice border to table, ugly but functional

--- grabReport.php
+++ grabReport.php
@@ -1,22 +1,21 @@
 <?php
-
 include "connection.php";
 
-$result = [];
-
 $reportSQL = "SELECT orderid, comments, shipdate_expected AS date,
-              IF(comments LIKE '%candy%', 'candy', IF(comments LIKE '%call%', 'contact preferences', IF((comments LIKE '%signature%' OR comments LIKE '%sign%'), 'Delivery Signature', IF(comments LIKE '%referred%', 'referrals', 'Miscellaneous')))) AS classification 
+              IF(comments LIKE '%candy%', 'candy', IF((comments LIKE '%call%' or comments LIKE '%contact%'), 'contact preferences', IF((comments LIKE '%signature%' OR comments LIKE '%sign%'), 'Delivery Signature', IF(comments LIKE '%referred%', 'referrals', 'Miscellaneous')))) AS classification 
               FROM sweetwater_test ORDER BY classification";
 
 $result = $conn->query($reportSQL);
-
 if ($result->num_rows > 0)
 {
-    echo "<table><tr><th>Order ID</th><th>Comments</th><th>Classification</th><th>Expected Delivery</th></tr>";
+    echo "<table border='1'><tr><th>Order ID</th><th>Comments</th><th>Classification</th><th>Expected Delivery</th></tr>";
     while($row = $result->fetch_assoc())
     {
        echo "<tr><td>" . $row['orderid'] . "</td><td style='text-align:center'>" . $row['comments'] . "</td><td>" . $row['classification'] . "</td><td>" . $row['date'] . "</td></tr>";
     }
     echo "</table>";
 }
+
+
+$conn->close();
 ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Comment report data</title>
+</head>
+<body>
+<?php
+include "setShippingDates.php";
+include "grabReport.php";
+?>
+</body>
+</html>
