ef94ae17a4dd02cbc8ccfd9573d13d2a1540cd76
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-02-12 17:03:33 -0500

    some ugly result formatting

--- grabReport.php
+++ grabReport.php
@@ -4,7 +4,7 @@ include "connection.php";
 
 $result = [];
 
-$reportSQL = "SELECT orderid, comments, 
+$reportSQL = "SELECT orderid, comments, shipdate_expected AS date,
               IF(comments LIKE '%candy%', 'candy', IF(comments LIKE '%call%', 'contact preferences', IF((comments LIKE '%signature%' OR comments LIKE '%sign%'), 'Delivery Signature', IF(comments LIKE '%referred%', 'referrals', 'Miscellaneous')))) AS classification 
               FROM sweetwater_test ORDER BY classification";
 
@@ -12,7 +12,11 @@ $result = $conn->query($reportSQL);
 
 if ($result->num_rows > 0)
 {
+    echo "<table><tr><th>Order ID</th><th>Comments</th><th>Classification</th><th>Expected Delivery</th></tr>";
     while($row = $result->fetch_assoc())
-    echo "<h1>order id: " . $row['orderid'] . " comment: " . $row['comments'] . "classification: " . $row['classification'] . "</h1>";
+    {
+       echo "<tr><td>" . $row['orderid'] . "</td><td style='text-align:center'>" . $row['comments'] . "</td><td>" . $row['classification'] . "</td><td>" . $row['date'] . "</td></tr>";
+    }
+    echo "</table>";
 }
 ?>
