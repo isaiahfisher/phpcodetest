24a85a289a3b1e1c92bbeefd5e5abee88351f7b1
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-02-12 16:33:28 -0500

    Add order by clause to report sql query -- rename variables to better describe the query

--- grabReport.php
+++ grabReport.php
@@ -4,13 +4,12 @@ include "connection.php";
 
 $result = [];
 
-$candySQL = "SELECT orderid, comments, IF(comments like '%candy%', 'candy', IF(comments like '%call%', 'contact purchaser', IF(comments like '%referred%', 'referrals', 'Miscellaneous'))) AS classification FROM sweetwater_test";
+$reportSQL = "SELECT orderid, comments, IF(comments like '%candy%', 'candy', IF(comments like '%call%', 'contact purchaser', IF(comments like '%referred%', 'referrals', 'Miscellaneous'))) AS classification FROM sweetwater_test ORDER BY classification";
 
-$result = $conn->query($candySQL);
+$result = $conn->query($reportSQL);
 
 if ($result->num_rows > 0)
 {
-    echo "<h1>hi</h1>";
     while($row = $result->fetch_assoc())
     echo "<h1>order id: " . $row['orderid'] . " comment: " . $row['comments'] . "classification: " . $row['classification'] . "</h1>";
 }
